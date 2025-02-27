83e5f2039659e1fddb1dceaee426e39a988399f4
Asiqul Islam <asiq.dev@gmail.com>
2025-02-27 15:09:17 +0600

    1 Two Sum problem solved

---
+++ Easy/1_two_sum.py
@@ -0,0 +1,22 @@
+# 1. Two Sum
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+# Example 1
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for index, value in enumerate(nums):
+            diff = target - value
+            if diff in seen:
+                return [seen[diff], index]
+            seen[value] = index
+
+obj = Solution()
+print(obj.twoSum([2, 5, 11, 15, 7], 9))
